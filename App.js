e1fea3044f3d029cc8d757a5cc0587d97039dc39
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2024-08-06 23:50:26 +0200

    added dist , parcel-cashe,

---
+++ App.js
@@ -0,0 +1,15 @@
+import ReactDom from 'react-dom';
+
+const App=()=>{
+    return(
+        <>
+         <h1>
+            React Working with parcel
+        </h1>
+        
+        </>
+       
+    )
+}
+
+ReactDom.render(<App/>,document.getElementById('root'));
