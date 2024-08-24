4112142aa2d49d4d657f46bc940543729c734455
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2024-08-24 15:13:11 +0200

    Accordian with Toggle feature

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import { CDN_URL } from '../utils/constant'
 
 const ItemList = ({items}) => {
-    console.log(items)
+    //console.log(items)
   return (
-    <div>
+    <div > 
             {items.map(item=>(
-            <div key={item.card.info.id} className='text-left p-2 m-2 border-b-2'>
-                <div className='p-2'>
-                    <span className='font-bold '>{item.card.info.name}</span>
-
+            <div key={item.card.info.id} className='flex justify-between text-left p-2 m-2 border-b-2'>
+                <div className='py-2 w-9/12'>
+                    <span className='font-bold text-md '>{item.card.info.name}</span>
                     <span> - ₹{item.card.info.price/100 || item.card.info.defaultPrice/100}</span>
+                <p className='text-xs py-2' >{item.card.info.description}</p>
                 </div>
-                <p className='text-xs ' >{item.card.info.description}</p>
+
+                <div className='w-3/12 p-4 '>
+                <div className='absolute'>
+                <button className='p-2  w-16 mx-12 my-24 cursor-pointer text-green-700 bg-white font-bold rounded-md shadow-lg'>Add+</button>
+                </div>
+                <img className=' rounded-md' src={CDN_URL+item.card.info.imageId } alt="" />
+                </div>
+                
             </div>
             ))}
     </div>
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import ItemList from './ItemList'
-
+import { useState } from 'react'
 const RestaurantCategory = (props) => {
+  
+  const [showItems,setShowItems] =useState(false);
+
+  const handleClick=()=>{
+    setShowItems(!showItems);
+  }
+
  // console.log(props)
   const {title,itemCards} =props.data
 
   return (
     <div className='w-6/12 bg-gray-100 shadow-lg my-3 p-4 mx-auto' >
-      {/* Header */}
       
-      <div className=' flex justify-between '>
-      <span className='font-bold text-lg'>{title} ({itemCards.length})</span>
-      <span>⬇️</span>
+      <div className=' flex justify-between cursor-pointer ' onClick={handleClick} >
+          <span className='font-bold text-lg'>{title} ({itemCards.length})</span>
+          <span>⬇️</span>
       </div>
-
-      {/*accordian  Body */}
-      {/* {
-        itemCards.map(items=><ItemList ></ItemList>)
-      } */}
-      <ItemList items={itemCards} ></ItemList>
-
-
-      
+      {showItems && <ItemList items={itemCards} ></ItemList>}
     </div>
   )
 }
